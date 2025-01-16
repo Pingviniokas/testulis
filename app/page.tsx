fcb15a313d28d79d3ee94cb382bb54ee6debe88b
Pingviniokas <tomas.budrys.tomas@gmail.com>
2025-01-16 06:16:52 +0300

    m

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
 import Hero from '@/components/Hero';
 
-export default function Home() {
+export default function PerkraustymoPage() {
   return (
     <main className="min-h-screen">
       <Hero />
-     
     </main>
   );
 }
